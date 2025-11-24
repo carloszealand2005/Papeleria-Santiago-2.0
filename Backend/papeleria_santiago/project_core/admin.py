1c2c48e73db88a52542c7d7387677d629d2d458d
carloszealand2005 <carldavid1105@gmail.com>
2025-11-23 22:29:57 -0500

    Backend en funcionamiento. ESTA VERSIÓN NO ESTÁ LISTA PARA PRODUCCIÓN YA QUE LA BASE FUE PUESTA EN .GITIGNORE

--- Backend/papeleria_santiago/project_core/admin.py
+++ Backend/papeleria_santiago/project_core/admin.py
@@ -7,8 +7,23 @@ from .models import (
 # Registra tus modelos aquí.
 admin.site.register(Cliente)
 admin.site.register(Producto)
-admin.site.register(Pedido)
 admin.site.register(Precio)
 admin.site.register(Inventario)
 admin.site.register(DetallePedido)
 admin.site.register(Transportista)
+
+
+# Personalizables: 
+# Mostrar el coste total de pedidos en tiempo real:
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'cliente', 'fecha_pedido', 'estado_pedido',
+        'monto_total_display' # Aquí incluimos la propiedad calculada
+    )
+   
+    # Calcular el monto total basado en los detalle_pedidos existentes que pertenezcan a este pedido. 
+    def monto_total_display(self, obj):
+        return f"${obj.monto_total:,.2f}" # Formateado como moneda
+    monto_total_display.short_description = "Monto Total" # Nombre de la columna en el admin
+
