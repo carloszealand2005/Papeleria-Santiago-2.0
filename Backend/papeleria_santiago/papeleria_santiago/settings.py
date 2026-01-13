51da6396fcd0927d51760e4cd02ff7030a7f8f6f
carloszealand2005 <carldavid1105@gmail.com>
2026-01-13 17:10:02 -0500

    Migración a postgreSQL, ver settings.py de backend para ver credenciales de base de datos

--- Backend/papeleria_santiago/papeleria_santiago/settings.py
+++ Backend/papeleria_santiago/papeleria_santiago/settings.py
@@ -140,9 +140,13 @@ WSGI_APPLICATION = 'papeleria_santiago.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'papeleria_santiago',
+        'USER': 'papeleria_user',
+        'PASSWORD': 'psSecure203',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
